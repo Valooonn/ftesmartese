342cd9fc7a09be180a44fce4cc4dd86cd677b8e8
Valooonn <ferativ21@gmail.com>
2026-07-02 18:10:13 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+
+
+const invitationData = {
+  studioName: "Foto Studio ART",
+  fotoArtInstagramName: "Foto Art Gostivar",
+  fotoArtInstagram: "https://www.instagram.com/fotoartgostivar/",
+  brideName: "Behari't", 
+  groomName: "Anesë's", 
+  eventInitials: "B & A",
+  weddingDate: "2026-08-03T20:00:00",
+  displayDate: "03 Gusht 2026",
+  displayTime: "20:00",
+  locationName: "Restorant De Luxe Arena",
+  address: "Gostivar, Maqedonia e Veriut",
+  googleMapsLink: "https://maps.google.com/",
+  music: "assets/music/wedding-music.mp3",
+  openingVideo: "assets/images/opening-video.mp4",
+  heroImage: "assets/images/hero.jpg",
+  heroVideo: "assets/images/video-dance.mp4",
+  galleryImages: [
+    "assets/images/photo1.jpg",
+    "assets/images/photo2.jpg",
+    "assets/images/photo3.jpg"
+  ],
+  
+  
+  designAssets: {
+    luxuryBackground: "assets/images/luxury-bg.png",
+    goldFrame: "assets/images/gold-frame.png",
+    floralCornerTop: "assets/images/floral-corner-top.png",
+    floralCornerBottom: "assets/images/floral-corner-bottom.png"
+  },
+  familyText: "Me gëzim ju ftojmë të merrni pjesë në dasmën e fëmijëve tanë.",
+  brideFamily: "Familja Musliji",
+  groomFamily: "Familja Ramadani",
+  giftInfo: "",
+  rsvpPhone: "38972666766",
+  contactPhone: "072 666 766",
+  introLine: "Me dashuri ju ftojmë të festoni me ne."
+};
+
+window.invitationData = invitationData;
